838b0396d720cd6f7ef28997814126396d5a4df5
Anna Kiniova <anna.kiniova@gmail.com>
2021-08-29 18:16:22 +0300

    Revert "task 3."
    This reverts commit ee752c1bdb9f72d805bda325041a352f1fa0bcfd.

--- product-service/src/errors.ts
+++
@@ -1,9 +0,0 @@
-export enum ErrorCode {
-  NOT_FOUND = 404,
-  BAD_REQUEST = 400,
-}
-
-export enum ErrorMessage {
-  NOT_FOUND = "Object not found",
-  BAD_REQUEST = "Bad request",
-}
--- product-service/src/functions/getProductList/handler.ts
+++
@@ -1,9 +0,0 @@
-import { formatJSONResponse } from "@libs/apiGateway";
-import { middyfy } from "@libs/lambda";
-import products from "../products.json";
-
-const getProductList = async () => {
-  return formatJSONResponse(products);
-};
-
-export const main = middyfy(getProductList);
